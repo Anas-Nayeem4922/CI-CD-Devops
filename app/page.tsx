1374e58f4908df781ab629445b22c8e43670a2a1
Anas Nayeem <anasnayeem97@gmail.com>
2025-02-07 23:22:28 +0530

    added basic next-js

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  return <div>
-    Hi there
+  return <div className="bg-red-300 h-screen w-full flex justify-center items-center">
+    <div className="text-5xl font-bold text-black">Hi there</div>
   </div>
 }
